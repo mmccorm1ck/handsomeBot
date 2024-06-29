43561a1eeadd18ff94a40a4c057a0b121a1bfbaf
Morgan McCormick <mmccorm1ck@outlook.com>
2024-06-29 10:46:58 +0100

    Set default window width and height

--- obj/Debug/net8.0/HandsomeBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/HandsomeBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1f9b009cda8977d7a799208e2fd7ce4378218f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b3a77a1c1ea5b153ff9752acbbf8f124adaf9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
