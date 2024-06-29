36e62f03be201b1b95cee5a8701d1f5a3205f094
Morgan McCormick <mmccorm1ck@outlook.com>
2024-06-29 21:56:08 +0100

    Implemented view switching

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime;
 using System.Runtime.CompilerServices;
+using ReactiveUI;
 
 namespace HandsomeBot.ViewModels;
 
@@ -15,6 +19,8 @@ public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 
     private ViewModelBase _currentPage = new BotTeamPageViewModel();
 
+    private int currentPageNumber = 0;
+
     public ViewModelBase currentPage
     {
         get => _currentPage;
@@ -24,4 +30,33 @@ public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
             OnPropertyChanged();
         }
     }
+
+    public ObservableCollection<PageNumberTemplate> PageNumberList { get; } = new()
+    {
+        new PageNumberTemplate(typeof(BotTeamPageViewModel), 0),
+        new PageNumberTemplate(typeof(OppTeamPageViewModel), 1)
+    };
+
+    public class PageNumberTemplate
+    {
+        public PageNumberTemplate(Type type, int pgNum)
+        {
+            ModelType = type;
+            pageNumber = pgNum; 
+        }
+
+        public int pageNumber { get; }
+        public Type ModelType { get; }
+    }
+
+    public void NextPage()
+    {
+        PageNumberTemplate targetPage = PageNumberList[currentPageNumber+1];
+        if (targetPage is null) return;
+        var instance = Activator.CreateInstance(targetPage.ModelType);
+        if (instance is null) return;
+        currentPage = (ViewModelBase)instance;
+        currentPageNumber++;
+    }
+
 }
