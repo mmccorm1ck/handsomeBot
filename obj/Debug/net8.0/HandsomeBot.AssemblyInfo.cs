61181c20002f86f6911836c5f9dedf6ba751eddc
Morgan McCormick <mmccorm1ck@outlook.com>
2024-06-30 21:14:42 +0100

    Made battle page layout

--- obj/Debug/net8.0/HandsomeBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/HandsomeBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1402cce37dfe36898d11ed50cc996ca5c7df91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4a58f53d08719a18939b6fd4f38c61905460e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HandsomeBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
