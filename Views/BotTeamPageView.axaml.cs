d394f9db6e3a482a9f90aae040c82fba679fa237
Morgan McCormick <mmccorm1ck@outlook.com>
2024-07-21 18:04:20 +0100

    Added httpClient to scrape pokepaste data

--- Views/BotTeamPageView.axaml.cs
+++ Views/BotTeamPageView.axaml.cs
@@ -1,24 +1,35 @@
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace HandsomeBot.Views;
 
 public partial class BotTeamPageView : UserControl
 {
+    
     public BotTeamPageView()
     {
         InitializeComponent();
     }
 
-    public void ConfirmTeam(object source, RoutedEventArgs args)
-    {
-        Debug.WriteLine("Click!");
-    }
-
     public void LoadPaste(object source, RoutedEventArgs args)
     {
-        Debug.WriteLine("Clock!");
+        if (pasteLink.Text is null)
+        {
+            return;
+        }
+        string httpLink = pasteLink.Text;
+        Task task = Task.Run(async () => await LoadPasteHtml(httpLink));
+    }
+    public async Task LoadPasteHtml(string httpLink)
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(httpLink);
+        Debug.WriteLine(response);
     }
 
     public void LoadPrevious(object source, RoutedEventArgs args)
@@ -26,3 +37,4 @@ public partial class BotTeamPageView : UserControl
         Debug.WriteLine("Clack!");
     }
 }
+
