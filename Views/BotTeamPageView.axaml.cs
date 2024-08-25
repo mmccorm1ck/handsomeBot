20fdf70632558b991c0413a08d614329f2f09a5e
Morgan McCormick <mmccorm1ck@outlook.com>
2024-08-25 18:08:11 +0100

    Began implementing pokepaste HTML parser

--- Views/BotTeamPageView.axaml.cs
+++ Views/BotTeamPageView.axaml.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace HandsomeBot.Views;
 
@@ -29,6 +30,81 @@ public partial class BotTeamPageView : UserControl
     {
         HttpClient client = new HttpClient();
         string response = await client.GetStringAsync(httpLink);
+        string[] responses = response.Split('\n');
+        int currPokemon = -1;
+        int currMove = -1;
+        string format;
+        for (int i = 0; i < responses.Length; i ++) {
+            responses[i] = responses[i].TrimStart(' ');
+            if (responses[i] == "<article>") {
+                currPokemon++;
+                continue;
+            }
+            if (currPokemon < 0) {
+                continue;
+            }
+            if (responses[i].Contains("Format")) {
+                format = responses[i].Split(' ')[1][0..^4];
+                continue;
+            }
+            if (responses[i].Contains("img-pokemon")) {
+                = "https://pokepast.es" + responses[i].Split(' ')[2][5..^2];
+                continue;
+            }
+            if (responses[i].Contains("Nature")) {
+                = responses[i].Split(' ')[0];
+                currMove = 0;
+                continue;
+            }
+            if (currMove != -1) {
+                if (responses[i] == "") {
+                    currMove = -1;
+                    continue;
+                }
+                int idx;
+                if (responses[i].Contains("IVs")) {
+                    responses[i] = responses[i][0..^7];
+                    idx = responses[i].LastIndexOf('>') + 1;
+                    = responses[i][idx..];
+                    continue;
+                }
+                idx = responses[i].LastIndexOf('>') + 1;
+                = responses[i][idx..].TrimStart([' ','-']);
+                currMove++;
+                if (currMove > 3) {
+                    currMove = -1;
+                }
+                continue;
+            }
+            if (!responses[i].Contains("<span")) {
+                continue;
+            }
+            if (responses[i].Contains("Ability")) {
+                int idx = responses[i].LastIndexOf('>') + 1;
+                = responses[i][idx..];
+                continue;
+            }
+            if (responses[i].Contains("Level")) {
+                int idx = responses[i].LastIndexOf('>') + 1;
+                = responses[i][idx..];
+                continue;
+            }
+            if (responses[i].Contains("Tera Type")) {
+                responses[i] = responses[i][0..^7];
+                int idx = responses[i].LastIndexOf('>') + 1;
+                = responses[i][idx..];
+                continue;
+            }
+            if (responses[i].Contains("EVs")) {
+                string[] temp = responses[i].Split(" / ");
+                for (int j = 0; j < temp.Length; j++) {
+                    temp[j] = temp[j][0..^7];
+                    int idx = temp[j].LastIndexOf('>') + 1;
+                    = temp[j][idx..]; 
+                }
+            }
+
+        }
         Debug.WriteLine(response);
     }
 
