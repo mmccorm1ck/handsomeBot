d23f08640c7d2b80ea7c48b387bae905883f740e
Morgan McCormick <mmccorm1ck@outlook.com>
2024-09-08 12:51:18 +0100

    Implemented game model to save game info eg format

---
+++ Models/GameModel.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+public class GameModel() : INotifyPropertyChanged // Class to hold info about a pokemon in a team
+{
+    public string Format
+    {
+        get => _format;
+        set
+        {
+            _format = value;
+            OnPropertyChanged();
+        }
+    }
+    public string BotTeamURL
+    {
+        get => _botTeamURL;
+        set
+        {
+            _botTeamURL = value;
+            OnPropertyChanged();
+        }
+    }
+    public string OppTeamURL
+    {
+        get => _oppTeamURL;
+        set
+        {
+            _oppTeamURL = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _format;
+    private string _botTeamURL;
+    private string _oppTeamURL;
+    public event PropertyChangedEventHandler PropertyChanged; // Event handler to update UI when variables change
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) // Function to trigger above event handler
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
