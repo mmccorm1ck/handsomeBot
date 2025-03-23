975fc9475f8bf57c5bf4954001198831813a56f1
Morgan McCormick <mmccorm1ck@outlook.com>
2025-03-23 10:58:20 +0000

    Add active mon tracking to turn model

--- Models/TurnModel.cs
+++ Models/TurnModel.cs
@@ -23,8 +23,48 @@ public class TurnModel() : INotifyPropertyChanged // Class to hold info about a 
             OnPropertyChanged();
         }
     }
+    public List<int> BotStartMons
+    {
+        get => _botStartMons;
+        set
+        {
+            _botStartMons = value;
+            OnPropertyChanged();
+        }
+    }
+    public List<int> BotEndMons
+    {
+        get => _botEndMons;
+        set
+        {
+            _botEndMons = value;
+            OnPropertyChanged();
+        }
+    }
+    public List<int> OppStartMons
+    {
+        get => _oppStartMons;
+        set
+        {
+            _oppStartMons = value;
+            OnPropertyChanged();
+        }
+    }
+    public List<int> OppEndMons
+    {
+        get => _oppEndMons;
+        set
+        {
+            _oppEndMons = value;
+            OnPropertyChanged();
+        }
+    }
     private int _turnNo;
     private List<Models.EventModel> _eventList = [];
+    private List<int> _botStartMons = [];
+    private List<int> _botEndMons = [];
+    private List<int> _oppStartMons = [];
+    private List<int> _oppEndMons = [];
     public event PropertyChangedEventHandler? PropertyChanged; // Event handler to update UI when variables change
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) // Function to trigger above event handler
     {
