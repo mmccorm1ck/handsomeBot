3b51753d507887b318d17c007efa78b70d70ce2a
Morgan McCormick <mmccorm1ck@outlook.com>
2025-10-10 12:10:11 +0100

    Made target selector notify when changed (i am blind apparently)

--- client/Models/TargetSelectorModel.cs
+++ client/Models/TargetSelectorModel.cs
@@ -4,50 +4,50 @@ using System.Runtime.CompilerServices;
 
 namespace HandsomeBot.Models;
 
-    public class TargetSelectorModel(int target)
+public class TargetSelectorModel(int target) : INotifyPropertyChanged
+{
+    private readonly int _targetNo = target;
+    private bool _selected = false;
+    public bool Selected
     {
-        private readonly int _targetNo = target;
-        private bool _selected = false;
-        public bool Selected
+        get => _selected;
+        set
         {
-            get => _selected;
-            set
-            {
-                _selected = value;
-                Update();
-                OnPropertyChanged();
-            }
-        }
-        private List<EventModel> events = [];
-        public void Attach(EventModel model)
-        {
-            events.Add(model);
-            foreach (EventModel ev in events)
-            {
-                Selected = ev.TargetMons.Contains(_targetNo);
-            }
-        }
-        public void Detach()
-        {
-            events = [];
-        }
-        private void Update()
-        {
-            foreach (EventModel ev in events)
-            {
-                if (Selected)
-                {
-                    if (!ev.TargetMons.Contains(_targetNo)) ev.TargetMons.Add(_targetNo);
-                }
-                else
-                {
-                    ev.TargetMons.Remove(_targetNo);
-                }
-            }
-        }
-        public event PropertyChangedEventHandler? PropertyChanged; // Event handler to update UI when variables change
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) // Function to trigger above event handler
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            _selected = value;
+            Update();
+            OnPropertyChanged();
         }
     }
+    private List<EventModel> events = [];
+    public void Attach(EventModel model)
+    {
+        events.Add(model);
+        foreach (EventModel ev in events)
+        {
+            Selected = ev.TargetMons.Contains(_targetNo);
+        }
+    }
+    public void Detach()
+    {
+        events = [];
+    }
+    private void Update()
+    {
+        foreach (EventModel ev in events)
+        {
+            if (Selected)
+            {
+                if (!ev.TargetMons.Contains(_targetNo)) ev.TargetMons.Add(_targetNo);
+            }
+            else
+            {
+                ev.TargetMons.Remove(_targetNo);
+            }
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged; // Event handler to update UI when variables change
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) // Function to trigger above event handler
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
