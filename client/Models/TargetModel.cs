0dea3e3d725ba59d2089d45a09163d9a10a0e9d0
Morgan McCormick <mmccorm1ck@outlook.com>
2025-12-15 10:17:13 +0000

    New model for holding target info

---
+++ client/Models/TargetModel.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HandsomeBot.Models;
+
+public class TargetModel(Dictionary<string, int> nameToNo) : INotifyPropertyChanged // Class for recording which pokemon are selected as targets in an event
+{
+    private readonly Dictionary<string, int> _nameToNo = nameToNo;
+    private string _monName = "";
+    public string MonName
+    {
+        get => _monName;
+        set
+        {
+            _monName = value;
+            _monNo = _nameToNo[value];
+            OnPropertyChanged();
+        }
+    }
+    private int _monNo = -1;
+    public int MonNo
+    {
+        get => _monNo;
+    }
+    private string _moveResult = "";
+    public string MoveResult
+    {
+        get => _moveResult;
+        set
+        {
+            _moveResult = value;
+            OnPropertyChanged();
+        }
+    }
+    private int? _damage;
+    public int? Damage
+    {
+        get => _damage;
+        set
+        {
+            _damage = value;
+            OnPropertyChanged();
+        }
+    }
+    private TeamModel _targetMonModel = new();
+    public void Attach(ImageListener listener)
+    {
+        _targetMonModel.Attach(listener);
+    }
+    public void Clear()
+    {
+        _targetMonModel.Clear();
+    }
+    public void Notify()
+    {
+        _targetMonModel.Notify();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged; // Event handler to update UI when variables change
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) // Function to trigger above event handler
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
